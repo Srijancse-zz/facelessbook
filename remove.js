2182e14b9de1eabbd761ae161042b7a7edf1d8b2
Kevin Chen <zephyrembyr@gmail.com>
2017-10-20 22:50:30 -0400

    add color

--- remove.js
+++ remove.js
@@ -3,3 +3,17 @@ var dict = {};
 elementList = document.querySelectorAll(".UFICommentActorName");
 
 console.log("yes")
+
+
+
+
+
+var COLORS = [];
+while (COLORS.length < 100) {
+    COLORS.push(`rgb(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0, 255)})`);
+}
+
+// random number generator
+function rand(frm, to) {
+    return ~~(Math.random() * (to - frm)) + frm;
+}
