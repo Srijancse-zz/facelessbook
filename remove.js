83a882aeb7d5636f35c104ff6e720f66ab55deef
Jane Manchun Wong <wongmjane@icloud.com>
2017-10-21 22:27:32 -0400

    Singleton class for caching generated color codes

--- remove.js
+++ remove.js
@@ -1,5 +1,35 @@
 {
 
+/**
+ * Singleton id <--> color mapping. It's like caching.
+ */
+class IdColorMap {
+  constructor() {
+    this.data = new Map();
+  }
+  static create() {
+    return new this();
+  }
+  static decToHex(dec) {
+    let hex = dec.toString(16);
+    while(hex.length < 6) hex = '0' + hex;
+    return hex;
+  }
+  get(hash) {
+    if(!this.data.has(hash)) {
+      this.reset(hash);
+    }
+    return IdColorMap.decToHex(this.data.get(hash));
+  }
+  reset(hash) {
+    this.data.set(hash, Math.floor((Math.abs(Math.sin(hash) * 16777215)) % 16777215));
+    return this;
+  }
+  toString() {
+    return String(this.data);
+  }
+}
+
 let selfStyleEl = document.querySelector("style#fbless") || (el => {
   el.id = "fbless";
   document.head.appendChild(el);
@@ -9,6 +39,7 @@ let selfStyleEl = document.querySelector("style#fbless") || (el => {
 let selfStyle = selfStyleEl.sheet;
 
 let knownElements = new Set();
+let colorMap = IdColorMap.create();
 
 function extractID(path) {
   return new URLSearchParams(path.substr(1+path.indexOf("?"))).get('id')
@@ -16,16 +47,6 @@ function extractID(path) {
 
 function getCurrrentDOM_Elements() {
   return Array.prototype.filter.call(document.querySelectorAll('[data-hovercard]'),el=>/^\/ajax\/hovercard\/(user|hovercard|group|page)\.php/.test(el.dataset.hovercard));
-}
-
-
-function genColor(seed) {
-  let color = Math.floor((Math.abs(Math.sin(seed) * 16777215)) % 16777215).toString(16);
-  // pad any colors shorter than 6 characters with leading 0s
-  while(color.length < 6) {
-      color = '0' + color;
-  }
-  return color;
 }
 
 // new Set(Array.prototype.filter.call(document.querySelectorAll('[data-hovercard]'),el=>/^\/ajax\/hovercard\/(user|hovercard|group)\.php/.test(el.dataset.hovercard)).map(el=>new URLSearchParams(el.dataset.hovercard.substr(1+el.dataset.hovercard.indexOf("?"))).get('id')))
@@ -42,10 +63,9 @@ updateKnownElements();
 
 knownElements.forEach(el => {
   let img;
-  el.style.background = `#${genColor(extractID(el.dataset.hovercard))}`;
+  el.style.background = `#${colorMap.get(extractID(el.dataset.hovercard))}`;
   el.style.color = "transparent";
   el.style.borderRadius = "1em"
-  // console.log(genColor(extractID(el.dataset.hovercard)));
   if(img = el.querySelector("img")) {
     img.style.opacity = "0";
   }
@@ -56,14 +76,3 @@ knownElements.forEach(el => {
 console.log("yes")
 
 }
-
-
-// var COLORS = [];
-// while (COLORS.length < 100) {
-//     COLORS.push(`rgb(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0, 255)})`);
-// }
-
-// // random number generator
-// function rand(frm, to) {
-//     return ~~(Math.random() * (to - frm)) + frm;
-// }
